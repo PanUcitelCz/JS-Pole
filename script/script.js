a8eb7c2c6df8950367723f944d99e952b59eefb5
DŠ-Dipex <dipex@seznam.cz>
2024-11-25 09:02:57 +0100

    hotovo

--- script/script.js
+++ script/script.js
@@ -13,16 +13,52 @@ function Add(){
     const content = document.getElementById("content");
     const user_cart = document.getElementById("user-cart");
 
-    if((input.value<nabidka.length) && (input.value> 0) && (!isNaN(input.value))){
+    if((input.value<=nabidka.length) && (input.value> 0) && (!isNaN(input.value))){
         kosik.push(nabidka[input.value -1]);  
-        nabidka.slice(input.value); // dodelat
+        nabidka.splice(input.value - 1, 1);
+        //nabidka.slice(input.value); // dodelat
     }
 
     else{
         alert("Chyba");
     }
     vypis(kosik, "user-cart");
+    vypis(nabidka, "content");
+    updateKosikOptions();
 
+}
+
+function updateKosikOptions(){ 
+    const select = document.getElementById("kosik-nabidka"); 
+    select.innerHTML = ""; 
+    
+    for(let i = 0; i < kosik.length; i++){ 
+        let option = document.createElement("option"); 
+        option.value = i + 1; 
+        option.text = kosik[i]; 
+        select.appendChild(option);
+    } 
+} 
+
+function Odeber(){ 
+    const select = document.getElementById("kosik-nabidka"); 
+    const index = select.selectedIndex;
+    
+    console.log("index = ", index)
+    
+    if(index >= 0){ 
+        nabidka.push(kosik[index]); 
+        kosik.splice(index, 1); 
+        updateKosikOptions(); 
+    } 
+    else { 
+        alert("Chyba");
+    } 
+    vypis(kosik, "user-cart"); 
+    vypis(nabidka, "content");
+
+    console.log(nabidka);
+    console.log(kosik);
 }
 
 vypis(nabidka, "content");
